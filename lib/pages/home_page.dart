8e50c687ddeb77bae1d368afeff865790aeec384
Wai Yan Tun Oo <62654761+WaiYanTunOo@users.noreply.github.com>
2022-03-09 18:01:29 +0630

    Improve the Mahabote UI with card view , checkbox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,9 +12,12 @@ class _HomePageState extends State<HomePage> {
   var selectedDate = DateTime.now();
   var selected = false;
   var f = DateFormat("dd/MM/yyyy EEEE");
-
   var modVal = 0;
   var houseName = "";
+  var myanmarYear = 0;
+  var numberMaharBote = '';
+  late DateTime picked;
+  var number = [1, 4, 0, 3, 6, 2, 5];
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +32,7 @@ class _HomePageState extends State<HomePage> {
 
   TextStyle _selectedColor(val) => TextStyle(
       color: houseName == val ? Theme.of(context).primaryColor : Colors.black);
-
+    
   Text _labelText(val) => Text(val, style: _selectedColor(val));
 
   Widget _mahaboteLayout() => Padding(
@@ -41,9 +44,9 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(""),
+                const Text(""),
                 _labelText("အဓိပတိ"),
-                Text(""),
+                const Text(""),
               ],
             ),
             Row(
@@ -73,6 +76,266 @@ class _HomePageState extends State<HomePage> {
     return houses[(year - day - 1) % 7];
   }
 
+ TextStyle _selectedColorNumber(val) => TextStyle(
+      color: numberMaharBote == val ? Theme.of(context).primaryColor : Colors.black);
+    
+  Text _labelNumber(val) => Text(val, style: _selectedColorNumber(val));
+
+
+  Widget _number0Layout() => Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(""),
+                _labelNumber('၄'),
+                const Text(""),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၆"),
+                _labelNumber("၂"),
+                _labelNumber("၅"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၃"),
+                _labelNumber('၀'),
+                _labelNumber("၁"),
+              ],
+            ),
+          ],
+        ),
+      );
+
+  Widget _number1Layout() => Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(""),
+                _labelNumber('၅'),
+                const Text(""),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၀"),
+                _labelNumber("၃"),
+                _labelNumber("၆"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၄"),
+                _labelNumber('၁'),
+                _labelNumber("၂"),
+              ],
+            ),
+          ],
+        ),
+      );
+
+Widget _number2Layout() => Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(""),
+                _labelNumber('၆'),
+                const Text(""),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၁"),
+                _labelNumber("၄"),
+                _labelNumber("၀"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၅"),
+                _labelNumber('၂'),
+                _labelNumber("၃"),
+              ],
+            ),
+          ],
+        ),
+      );
+      Widget _number3Layout() => Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(""),
+                _labelNumber('၀'),
+                const Text(""),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၂"),
+                _labelNumber("၅"),
+                _labelNumber("၁"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၆"),
+                _labelNumber('၃'),
+                _labelNumber("၄"),
+              ],
+            ),
+          ],
+        ),
+      );
+
+Widget _number4Layout() => Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(""),
+                _labelNumber('၁'),
+                const Text(""),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၃"),
+                _labelNumber("၆"),
+                _labelNumber("၂"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၀"),
+                _labelNumber('၄'),
+                _labelNumber("၅"),
+              ],
+            ),
+          ],
+        ),
+      );
+Widget _number5Layout() => Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(""),
+                _labelNumber('၂'),
+                const Text(""),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၄"),
+                _labelNumber("၀"),
+                _labelNumber("၃"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၁"),
+                _labelNumber('၅'),
+                _labelNumber("၆"),
+              ],
+            ),
+          ],
+        ),
+      );
+Widget _number6Layout() => Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(""),
+                _labelNumber('၃'),
+                const Text(""),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၅"),
+                _labelNumber("၁"),
+                _labelNumber("၄"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _labelNumber("၂"),
+                _labelNumber('၆'),
+                _labelNumber("၀"),
+              ],
+            ),
+          ],
+        ),
+      );
+
+  String _numberResult(year, day) {
+    var number = ['၁', '၂', '၃', '၄', '၅', '၆', '၀'];
+    return number[modVal];
+  }
+
   var isChecked = false;
   //1,4,0,3,6,2,5
   Widget _homeDesign() => ListView(
@@ -82,52 +345,52 @@ class _HomePageState extends State<HomePage> {
             color: Theme.of(context).primaryColor,
             child: ElevatedButton(
                 onPressed: () async {
-                  final DateTime? picked = await showDatePicker(
+                  picked = (await showDatePicker(
                       context: context,
                       initialDate: selectedDate,
-                      firstDate: DateTime(1800),
+                      firstDate: DateTime(1900),
                       lastDate: DateTime(2025),
                       helpText: 'Select Your Birthday', // Can be used as title
-                      cancelText: 'Not now');
-                  if (picked != null) {
-                    var myanmarYear = picked.year - 638;
-                    if (picked.month < 4) {
-                      myanmarYear = picked.year - 637;
-                    }
-
-                    setState(() {
-                      selectedDate = picked;
-                      modVal = myanmarYear % 7;
-                      houseName = _houseResult(myanmarYear, picked.weekday);
-                      selected = true;
-                    });
-                  }
+                      cancelText: 'Not now'))!;
+                  setState(() {
+                    changeMyanmarYear(picked);
+                  });
                 },
                 child: selected
                     ? Text(f.format(selectedDate))
                     : const Text("Select Your Birthday")),
           ),
-          // Checkbox(
-          //   checkColor: Colors.white,
-          //   value: isChecked,
-          //   onChanged: (bool? value) {
-          //     setState(() {
-          //       isChecked = value!;
-          //     });
-          //   },
-          // ),
-          Container(
-            margin: const EdgeInsets.all(12),
-            height: 150,
-            child: Card(child: Center(child: _mahaboteLayout())),
-          ),
+          selectedDate.month == 4
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      checkColor: Colors.white,
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                          changeMyanmarYear(picked);
+                        });
+                      },
+                    ),
+                    const Text('မြန်မာ နှစ်သစ်ကူးပြီးမှ မွေးဖွားသည်။')
+                  ],
+                )
+              : Container(),
           selected == false
               ? Container()
               : Container(
-                  margin: const EdgeInsets.all(12),
-                  child: Card(
-                      child: Padding(
-                          padding: const EdgeInsets.all(12),
+                  margin: const EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        height: 150,
+                        child: Card(child: Center(child: _mahaboteLayout())),
+                      ),
+                      Card(
+                        child: Center(
                           child: Column(
                             children: [
                               Text("အကြွင်း $modVal"),
@@ -138,8 +401,38 @@ class _HomePageState extends State<HomePage> {
                                     color: Theme.of(context).primaryColor),
                               ),
                             ],
-                          ))),
-                )
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        height: 150,
+                        child: Card(
+                          child: Center(
+                            child: modVal == 0 ? _number0Layout() : modVal==1?_number1Layout(): modVal==2?_number2Layout(): modVal==3?_number3Layout(): modVal==4?_number4Layout(): modVal==5?_number5Layout(): modVal==6?_number6Layout():Container(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
         ],
       );
+
+  void changeMyanmarYear(picked) {
+    if (picked != null) {
+      myanmarYear = picked.year - 638;
+      if (picked.month < 4) {
+        myanmarYear = picked.year - 637;
+      }
+      isChecked == false && picked.month == 4 ? myanmarYear -= 1 : myanmarYear;
+      setState(() {
+        selectedDate = picked;
+        modVal = myanmarYear % 7;
+        houseName = _houseResult(myanmarYear, picked.weekday);
+        numberMaharBote = _numberResult(myanmarYear, picked.weekday);
+        selected = true;
+      });
+    }
+  }
 }
